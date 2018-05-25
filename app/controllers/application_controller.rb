ddf8dbf512779a5ca79912080eb3cba1cb91c835
Ryan Mok <ryanm@nulogy.com>
2018-05-25 14:51:55 -0400

    Add iframe to main page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
   def index
-    render html: "<iframe width=\"350\" height=\"430\" src=\"https://console.dialogflow.com/api-client/demo/embedded/nuglossary\"></iframe>"
+    render html: "<iframe width=\"90%\" height=\"90%\" src=\"https://console.dialogflow.com/api-client/demo/embedded/nuglossary\"></iframe>".html_safe
   end
 end
