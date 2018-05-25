f5472d648bdabd3a40657e056572267823a8e18f
Ryan Mok <ryanm@nulogy.com>
2018-05-25 11:56:14 -0400

    Change route for webhook

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
-    def webhook
-        render html: "Welcome to webhook"
-    end
+  def webhook
+    render html: "Welcome to webhook"
+  end
 end
