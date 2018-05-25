97d4a5848b58f63eeaed4f991179b9eee13ee2e4
Ryan Mok <ryanm@nulogy.com>
2018-05-25 13:47:33 -0400

    Extract term from request

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -2,8 +2,11 @@ class WebhookController < ApplicationController
   def webhook
     render status: 401 unless request.headers["Authorization"] == ENV["API_ACCESS_KEY"]
     
+    request_object = JSON.parse request.body.read
+    term = request_object["result"]["parameters"]["nu_lookup"]
+
     render json: {
-      speech: "The JSON body is: #{request.body.read}."
+      speech: "The term you gave me is #{term}"
     }
   end
 end
