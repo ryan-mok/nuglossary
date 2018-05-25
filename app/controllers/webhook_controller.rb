c44272d3f039db3169c3bf8e30d58c83956f8ad5
Ryan Mok <ryanm@nulogy.com>
2018-05-25 13:32:02 -0400

    Return speech

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -1,11 +1,7 @@
 class WebhookController < ApplicationController
   def webhook
-    client = ApiAiRuby::Client.new(
-      :client_access_token => ENV['API_ACCESS_TOKEN']
-    )
-
-    response = client.text_request "glossary_lookup", { displayText: 'Test' }
-
-    render json: response
+    return json {
+      speech: "This is the webhook returning."
+    }
   end
 end
