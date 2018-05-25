42718699edc3c2c3c1e5bd8dc1957a1fabfdad38
Ryan Mok <ryanm@nulogy.com>
2018-05-25 15:30:13 -0400

    WIP: add Wikipedia API call

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -22,7 +22,7 @@ class WebhookController < ApplicationController
       )
       wikipedia = JSON.parse wikipedia_json
 
-      p wikipedia
+      p wikipedia[0][1]
 
       response = "#{nu_lookupTerm} is short for #{full_term}."
     end
