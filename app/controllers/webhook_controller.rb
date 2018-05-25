79107720c4ef8b454ea166f8ffd792b5111f38ac
Ryan Mok <ryanm@nulogy.com>
2018-05-25 13:21:31 -0400

    Change response

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -4,9 +4,7 @@ class WebhookController < ApplicationController
       :client_access_token => ENV['API_ACCESS_TOKEN']
     )
 
-    response = client.text_request "hello!"
-
-    p response
+    response = client.text_request "glossary_lookup", { displayText: 'Test' }
 
     render json: response
   end
